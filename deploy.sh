050514e0baf316ce0fe9ed0a3ebf0b7503ad6d26
turing <aleqvids@gmail.com>
2022-10-22 13:25:49 +0300

    updates

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:partylikeits1983/binaural_beats.git main:gh-pages
 
 cd -
